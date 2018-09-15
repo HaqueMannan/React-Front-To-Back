68fd2c0871920565df5cb4e58f7a8575daff3b7d
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-09-15 01:11:22 +0100

    Section 2 - Components, JSX & Props - 2.9. Adding Bootstrap.

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
 
 class App extends Component {
    render() {
       return (
          <div className="App">
             <Header branding="Contact Manager" />
-            <Contact name="John Doe" email="jdoe@gmail.com" phone="1111-2222222"/>
-            <Contact name="Emily Parker" email="eparker@gmail.com" phone="1111-3333333"/>
+            <div className="container">
+               <Contact name="John Doe" email="jdoe@gmail.com" phone="1111-2222222"/>
+               <Contact name="Emily Parker" email="eparker@gmail.com" phone="1111-3333333"/>
+            </div>
          </div>
       );
    }
