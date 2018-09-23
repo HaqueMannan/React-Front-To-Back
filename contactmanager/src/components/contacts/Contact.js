6f2bbd9919d2f8ce3a5c9460b8a0d2a7ce26f5ce
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-09-23 16:02:02 +0100

    Section 6 - Lifecycle, HTTP & Deployment - 6.5. EditContact Component & Fetch Contact.

--- contactmanager/src/components/contacts/Contact.js
+++ contactmanager/src/components/contacts/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Consumer } from '../../context';
 import axios from 'axios';
@@ -31,6 +32,15 @@ class Contact extends Component {
                      <h4>{name}
                         <i onClick = {() => this.setState({ showContactInfo: !this.state.showContactInfo })} className="fas fa-sort-down" style={{ cursor: "pointer" }}></i>
                         <i className="fas fa-times" style={{ cursor: "pointer", float: "right", color: "red" }} onClick = {this.onDeleteClick.bind(this, id, dispatch)}></i>
+                        <Link to={`contact/edit/${id}`}>
+                           <i className="fas fa-pencil-alt"
+                           style={{
+                              cursor: 'pointer',
+                              float: 'right',
+                              color: 'black',
+                              marginRight: '1rem'
+                           }}></i>
+                        </Link>
                      </h4>
                      {showContactInfo ? (
                      <ul className="list-group">
