87b9de718490abd09f1440b32d2e6bee95119c78
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-10-06 02:34:51 +0100

    Section 9 - Client Panel Firebase Project - 9.6. Client Details Component.

--- Section 9 - Client Panel Firebase Project/clientpanel_react/src/components/clients/Clients.js
+++ Section 9 - Client Panel Firebase Project/clientpanel_react/src/components/clients/Clients.js
@@ -62,7 +62,7 @@ class Clients extends Component {
                            <td>{client.email}</td>
                            <td>£{parseFloat(client.balance).toFixed(2)}</td>
                            <td>
-                              <Link to={`/clients/${client.id}`} className="btn btn-secondary btn-sm">
+                              <Link to={`/client/${client.id}`} className="btn btn-secondary btn-sm">
                                  <i className="fas fa-arrow-circle-right"/> Details
                               </Link>
                            </td>
