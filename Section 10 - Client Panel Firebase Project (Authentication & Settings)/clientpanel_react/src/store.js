ff72cbc83dffa13e181efb3cbaa55bd2f235a5e3
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-10-07 13:52:43 +0100

    Section 10 - Client Panel Firebase Project (Authentication & Settings) - 10.4. Notify State & Alert Component.

--- Section 10 - Client Panel Firebase Project (Authentication & Settings)/clientpanel_react/src/store.js
+++ Section 10 - Client Panel Firebase Project (Authentication & Settings)/clientpanel_react/src/store.js
@@ -4,7 +4,7 @@ import 'firebase/firestore';
 import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 //Custom Reducers:
-// @todo
+import notifyReducer from './reducers/notifyReducer';
 
 
 //Add Firebase to WebApp
@@ -39,7 +39,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
    firebase: firebaseReducer,
-   firestore: firestoreReducer
+   firestore: firestoreReducer,
+   notify: notifyReducer
 });
 
 // Create initial state
