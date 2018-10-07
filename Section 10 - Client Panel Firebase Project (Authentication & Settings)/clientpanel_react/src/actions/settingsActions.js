0d07f5fcb1d704292d9c27ada6d664447f35bfc3
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-10-07 15:22:49 +0100

    Section 10 - Client Panel Firebase Project (Authentication & Settings) - 10.7. Persist Settings To LocalStorage.

--- Section 10 - Client Panel Firebase Project (Authentication & Settings)/clientpanel_react/src/actions/settingsActions.js
+++ Section 10 - Client Panel Firebase Project (Authentication & Settings)/clientpanel_react/src/actions/settingsActions.js
@@ -1,19 +1,49 @@
-import { DISABLE_BALANCE_ON_ADD, DISABLE_BALANCE_ON_EDIT, ALLOW_REGISTRATION } from './types';
+import { ALLOW_REGISTRATION, DISABLE_BALANCE_ON_ADD, DISABLE_BALANCE_ON_EDIT } from './types';
+
+export const setAllowRegistration = () => {
+   //Get settings from localStorage
+   const settings = JSON.parse(localStorage.getItem('settings'));
+
+   //Toggle settings
+   settings.allowRegistration = !settings.allowRegistration;
+
+   //Set back to localStorage
+   localStorage.setItem('settings', JSON.stringify(settings))
+
+   return {
+      type: ALLOW_REGISTRATION,
+      payload: settings.allowRegistration
+   };
+};
 
 export const setDisableBalanceOnAdd = () => {
+   //Get settings from localStorage
+   const settings = JSON.parse(localStorage.getItem('settings'));
+
+   //Toggle settings
+   settings.disableBalanceOnAdd = !settings.disableBalanceOnAdd;
+
+   //Set back to localStorage
+   localStorage.setItem('settings', JSON.stringify(settings))
+
    return {
-      type: DISABLE_BALANCE_ON_ADD
+      type: DISABLE_BALANCE_ON_ADD,
+      payload: settings.disableBalanceOnAdd
    };
 };
 
 export const setDisableBalanceOnEdit = () => {
-   return {
-      type: DISABLE_BALANCE_ON_EDIT
-   };
-};
+   //Get settings from localStorage
+   const settings = JSON.parse(localStorage.getItem('settings'));
 
-export const setAllowRegistration = () => {
+   //Toggle settings
+   settings.disableBalanceOnEdit = !settings.disableBalanceOnEdit;
+
+   //Set back to localStorage
+   localStorage.setItem('settings', JSON.stringify(settings))
+
    return {
-      type: ALLOW_REGISTRATION
+      type: DISABLE_BALANCE_ON_EDIT,
+      payload: settings.disableBalanceOnAdd
    };
 };
